223ca67b2d65352f28c535483e9df90535b73daf
aanadhila <aanadhila225@gmail.com>
2022-03-18 15:35:15 +0700

    first commit

---
+++ app/Http/Controllers/PegawaiController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Pegawai;
+
+class PegawaiController extends Controller
+{
+     public function index()
+    {
+    	// mengambil data pegawai yang memiliki id 1
+        $pegawai = Pegawai::where('id', 1)->get(); 
+    
+        // mengirim data pegawai ke view pegawai 
+        return view('pegawai', ['pegawai' => $pegawai]); 
+
+        // mengambil data pegawai yang id nya lebih besar dari 10
+        // $pegawai = Pegawai::where('id', '>' , 10)->get();
+        // // mengambil data pegawai yang id nya lebih besar sama dengan 10 
+        // $pegawai = Pegawai::where('id', '>=' , 10)->get();
+        // // mengambil data pegawai yang di namanya ada huruf a 
+        // $pegawai = Pegawai::where('nama', 'like' , '%a%')->get();
+        // // menampilkan 10 data pegawai per halaman
+        // $pegawai = Pegawai::paginate(10);
+    }
+}
---
+++ database/seeders/PegawaiSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PegawaiSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       // data faker indonesia
+       $faker = Faker::create('id_ID');
+ 
+       // membuat data dummy sebanyak 10 record
+       for($x = 1; $x <= 10; $x++){
+
+           // insert data dummy pegawai dengan faker
+           DB::table('pegawai')->insert([
+               'nama' => $faker->name,
+               'alamat' => $faker->address,
+           ]);
+
+       } 
+    }
+}
